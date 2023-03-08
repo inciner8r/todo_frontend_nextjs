ac4251e00138936ae4ac83ad6768b47ca3b750ca
inciner8r <punarvpawade17@gmail.com>
2023-03-09 01:11:23 +0530

    structure

---
+++ pages/blogs/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Blog from "../components/blog";
+
+const Blogs = () => {
+  return (
+    <div className="blogs-page">
+      <Blog />
+      <Blog />
+      <Blog />
+    </div>
+  );
+};
+
+export default Blogs;
---
+++ pages/components/blog.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Blog = () => {
+  return (
+    <div className="blog">
+      <div className="title">Title</div>
+      <div className="content">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rem eos at
+        eveniet voluptatibus sed commodi libero tempore perspiciatis
+        necessitatibus laborum neque soluta, illum hic nam nisi corporis est
+        nihil deleniti!
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
