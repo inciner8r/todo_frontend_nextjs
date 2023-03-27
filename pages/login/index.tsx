948845115dc26589179fd4e7cbca5fd3416872cb
inciner8r <punarvpawade17@gmail.com>
2023-03-27 15:45:51 +0530

    login page

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-const Register = () => {
+const Login = () => {
   return (
     <div className="register-container items-center flex flex-col">
-      <div className="register-title my-10 text-3xl">Register</div>
+      <div className="register-title my-10 text-3xl">Login</div>
       <div className="register-container border-solid border-black border-2 pb-3 px-5">
         <div className="form flex flex-col">
           <label htmlFor="username" className="mt-3 ml-1">
@@ -12,15 +12,6 @@ const Register = () => {
           <input
             type="text"
             name="username"
-            id=""
-            className="border-solid border-black border-2 rounded-lg px-2 py-1"
-          />
-          <label htmlFor="email" className="mt-3 ml-1">
-            Email
-          </label>
-          <input
-            type="text"
-            name="email"
             id=""
             className="border-solid border-black border-2 rounded-lg px-2 py-1"
           />
@@ -37,7 +28,7 @@ const Register = () => {
             type="submit"
             className="submit flex items-start mt-5 border-solid border-2 bg-slate-500 w-fit p-2 rounded-xl"
           >
-            Register
+            Login
           </button>
         </div>
       </div>
@@ -45,4 +36,4 @@ const Register = () => {
   );
 };
 
-export default Register;
+export default Login;
